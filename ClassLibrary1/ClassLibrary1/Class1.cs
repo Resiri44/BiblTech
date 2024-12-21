9a8a17f920ab81ac2f68a22742e2315cbbcaa28e
Resiri44 <131768614+Resiri44@users.noreply.github.com>
2024-12-21 13:04:49 +0300

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValidationLibrary
+{
+    public static class InputValidator
+    {
+        // Проверка на числовое значение (int)
+        public static bool ValidateInt(string input)
+        {
+            return int.TryParse(input?.Trim(), out _);
+        }
+
+        // Проверка на числовое значение (decimal)
+        
+
+        public static bool ValidateDecimal(string input)
+        {
+        return decimal.TryParse(input?.Trim(), NumberStyles.Any, CultureInfo.InvariantCulture, out _);
+        }
+
+         // Проверка на корректную дату
+        public static bool ValidateDate(string input)
+        {
+            return DateTime.TryParse(input?.Trim(), out _);
+        }
+
+        // Проверка на пустую строку
+        public static bool ValidateNotEmpty(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input);
+        }
+    }
+}
